0feeaeaa35b478289d9862eacff89040875022c6
Rekha-Keethi <rekha.nkk5@gmail.com>
2026-03-15 21:09:56 +0100

    updated variable name

--- Assignments/13March2026/FactorialProblems/TC01FacotrialOfNumber.js
+++ Assignments/13March2026/FactorialProblems/TC01FacotrialOfNumber.js
@@ -1,10 +1,11 @@
 //Write a program to find the factorial of a given number using a loop.
 
+
 function factorialOfGivenNumber(n){
-    var fact = 1;
+    var factorial = 1;
     for ( let i=n ; i>=1 ; i-- )
     {
-        fact = fact*i;
+        factorial = factorial*i;
     }
     console.log("Factorial of ", n, " is ", fact)
 }
